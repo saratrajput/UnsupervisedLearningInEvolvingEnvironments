8b08b0989638fd514cbed385fa9e22e2734d0014
saratrajput <pattarsuraj@gmail.com>
2017-07-23 17:56:11 +0200

    zsolt14data

--- angelov/showdata.m
+++ angelov/showdata.m
@@ -1,5 +1,5 @@
 clear
-X = load('data.txt');
+X1 = load('data.txt');
 % for i=1:50:length(X)-100
 % plotdata(X(i:i+200,:));
 % axis([min(X(:,1)) max(X(:,1)) min(X(:,2)) max(X(:,2))]);
@@ -7,41 +7,42 @@ X = load('data.txt');
 % pause(.1);
 % end
 diste = [];
-for i = 1:size(X,1)-1
-    diste = [diste; abs(X(i)-X(i+1))];
+X1 = X1(1:14,1);
+for i = 1:size(X1,1)-1
+    diste = [diste; abs(X1(i)-X1(i+1))];
 end
 
 sum_diste = sum(diste);
 
 %% proximity
 % distance from one point to every other point
-row_num = size(X, 1);
-col_num = size(X, 1);
-prox = zeros(row_num, col_num);
+row_num = size(X1, 1);
+col_num = size(X1, 1);
+proxi = zeros(row_num, col_num);
 
-for j = 1:size(X,1)
-    for k = 1 : size(X, 1)
-        prox(j, k) = [abs(X(k) - X(j))];
+for j = 1:size(X1,1)
+    for k = 1 : size(X1, 1)
+        proxi(j, k) = [abs(X1(k) - X1(j))];
     end
 end
 
 %% accumulated proximity
 % sum of distaces to each data point from every point
 proxsum = [];
-for i = 1 : size(X, 1)
-    proxsum = [proxsum; sum(prox(:,i))];
+for i = 1 : size(X1, 1)
+    proxsum = [proxsum; sum(proxi(:,i))];
 end
 
 %% eccentricity
 % ((2 * accumlated proximity(j)) / sum of all accumulated proximities)
 eccen = [];
-for i = 1: size(X, 1)
+for i = 1: size(X1, 1)
     eccen = [eccen; (2 * proxsum(i)) / sum(proxsum)];
 end
 
 %% typicality
 
 typic = [];
-for i = 1 : size(X, 1)
+for i = 1 : size(X1, 1)
     typic = [typic; 1 - eccen(i)];
 end
