7428378fca7595b58faf97e5e147f801121aabd5
valikund <valikund@users.noreply.github.com>
2017-07-26 13:36:08 +0200

    Updated for all stuff

--- testingFile.m
+++ testingFile.m
@@ -1,5 +1,8 @@
-X = linspace(1,14,14)
-Y = [20.2;3;6.4;11.6;8.2;2.2;11.2;5.2;6.2;0.2;1.0;4.8;2.4;3.8]
+X = linspace(1,31,31)
+Y = [20.2;3;6.4;11.6;8.2;2.2;11.2;5.2;6.2;0.2;1.0;4.8;2.4;3.8;11;9.2;3.2;11.8;0.2;0.2;4.8;0.2;1;8.8;2.6;9.4;6.6;5.6;0.2;0.2;15.6]
+n = 1
+%X = [1,2,3,4]
+%Y = [10,3,4,5]
 scatter(X,Y)
 
 %calculating accumulated proximity
@@ -15,5 +18,21 @@ end
 %calculating eccentricity
 eccentr = []
 for i=1:length(prox)
-    eccentr = [eccentr;(2*prox(i))/(sum(prox))]
+    eccentr = [eccentr;prox(i)/(sum(prox))]
 end
+
+%Check anomaly
+gap = n/length(Y)
+sorted = sort(eccentr,'descend')
+anomaly = []
+for i=1:(length(Y)-1)
+    if (eccentr(i) -eccentr(i+1)) > gap
+        for j=1:i
+            anomaly = [anomaly;[i,eccentr(i)]]
+        end
+    end
+end
+
+scatter(X,eccentr,'filled')
+hold on
+scatter(anomaly(:,1),anomaly(:,2),'filled')
