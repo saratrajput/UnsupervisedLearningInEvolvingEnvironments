cdb8aae6da230acaa07a48e398f9165636a82a27
valikund <valikund@users.noreply.github.com>
2017-07-27 19:07:12 +0200

    Fixed the eccentricity sorting
    This is a final version

--- anomaly2D.m
+++ anomaly2D.m
@@ -1,13 +1,13 @@
 
 
-X = X(1:300,:)
+X = X;
 n = 1
 %X = [1,2,3,4]
 %Y = [10,3,4,5]
 %scatter(X(:,1),X(:,2))
 
 %calculating accumulated proximity
-prox = []
+prox = [];
 for i=1:length(X)
     pi = 0;
     for j=1:length(X)
@@ -17,27 +17,29 @@ for i=1:length(X)
 end
     
 %calculating the vanila eccentricity
-eccentr = []
+eccentr = [];
 for i=1:length(prox)
     eccentr = [eccentr;[i,2*prox(i)/(sum(prox))]];
 end
 
 %calculating the normalized eccentricity
 sigma_square = [std(X(:,1),1)^2,std(X(:,2),1)^2];
-eccentr = []
+eccentr = [];
 for i=1:length(prox)
     eccentricity = ((X(i,1)-mean(X(:,1)))^2)/(2*length(X)*sigma_square(1)) + ((X(i,2)-mean(X(:,2)))^2)/(2*length(X)*sigma_square(2)) + 1/(2*length(X));
-    eccentr = [eccentr;[i,eccentricity]]
+    eccentr = [eccentr;[i,eccentricity]];
 end
 
 %Check anomaly
 gap = n/length(X)
-sorted = sort(eccentr,'descend')
-anomaly = []
+[sorted, index] = sortrows(eccentr,2);
+anomaly = [];
 for i=1:(length(X)-1)
-    if (eccentr(i) -eccentr(i+1)) > gap
+    if (sorted(length(X)+1-i,2) -sorted(length(X)-i,2)) > gap
         for j=1:i
-            anomaly = [anomaly;[i,eccentr(i)]]
+            % in world coordinates this is the point
+            %anomaly = [anomaly;X(sorted(length(X)+1-j,1),:)]
+            anomaly = [anomaly;sorted(length(X)+1-j,:)]
         end
     end
 end
@@ -45,4 +47,5 @@ end
 figure(1)
 scatter(eccentr(:,1),eccentr(:,2),'filled')
 hold on
+%hline = refline([0 mean(eccentr)])
 scatter(anomaly(:,1),anomaly(:,2),'filled')
